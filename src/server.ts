380473e8bb0ca345155780f59f67eb8acfe348be
Amin Amgad <aminaminaminaminamin10@gmail.com>
2025-05-19 00:41:40 +0200

    edit

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,9 @@ connectDB();
 app.use(express.json());
 app.use("/api/auth", authRoutes)
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
